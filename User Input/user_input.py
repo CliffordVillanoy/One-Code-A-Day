4ed957eb2e510e13f0194d67eb652377b2fe1e87
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-05-21 22:51:50 +0800

    User Input update

---
+++ User Input/user_input.py
@@ -0,0 +1,8 @@
+first_name  = input("Enter your first name: ")
+last_name   = input("Enter your last name: ")
+print (f"Welcome {first_name} {last_name}!")
+# The above code takes the user's first and last name as input and prints a welcome message.
+# The input() function is used to take input from the user.
+# The f-string is used to format the string and insert the values of first_name and last_name into the string.
+
+

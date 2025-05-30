8cbb94f785a165e122b1075eb344f6ce1f590a5a
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-05-30 21:53:39 +0800

    Account Simulator

---
+++ Account Simulator/account_simulator.py
@@ -0,0 +1,39 @@
+"""
+bank_account_simulator.py
+
+This script simulates basic bank account transactions using Python variables, types, and control structures.
+
+Naming Convention: snake_case (PEP8)
+"""
+
+account_holder =  {
+    "name": "John Doe",
+    "account_number": "123456789",
+    "balance": 1000.0, #Float
+    "is_active": True #Boolean
+}
+
+transactions = [2000.00, -300.00, 210.00, -70.00]  # List to store transaction history
+
+print("\n===== BANK ACCOUNT TRANSACTION LOG =====\n")
+
+for txn in transactions:
+    if not account_holder["is_active"]:
+        print(f"Account {account_holder['account_number']} is inactive. Cannot process transactions.")
+        break
+
+    txn_type = "Deposit" if txn > 0 else "Withdrawal"
+    projected_balance = account_holder["balance"] + txn
+
+    if projected_balance < 0:
+        print (f"{txn_type} of ₱{abs(txn):.2f} failed. Insufficient funds for this transaction.")
+
+    else:
+        account_holder["balance"] = projected_balance
+        print(f"{txn_type} of ₱{abs(txn):.2f} successful.")
+        print(f"New balance: ₱{account_holder['balance']:.2f}")
+
+
+
+print("===== END OF TRANSACTION LOG =====")
+print(f"Final Balance for {account_holder['name']}: ₱{account_holder['balance']:.2f}\n")

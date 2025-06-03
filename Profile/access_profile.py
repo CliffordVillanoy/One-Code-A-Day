c6faaffd54038a811f1753e94780f9e7652ccde5
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-06-03 21:19:29 +0800

    Access Profile

---
+++ Profile/access_profile.py
@@ -0,0 +1,12 @@
+user = {
+    "name": "Cliff",
+    "role": "Junior SQA",
+    "is_active": True,
+    "interests": ["testing", "AI", "Automation"]
+}
+
+print(user["name" ])
+print(user["interests"][1])
+
+del user["is_active"]
+

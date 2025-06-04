dca8e03b9645c216b0932449e3132786b945b1b5
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-06-04 23:52:49 +0800

    Print Dictionary

---
+++ Print Dictionary/print_dictionary.py
@@ -0,0 +1,12 @@
+book = {
+    "title": "Clean Code",
+    "author": "Robert C. Martin",
+    "pages": 464,
+    "is_available": True
+}
+
+print("\n===== BOOK DETAILS =====\n")
+print(f"Title: {book['title']}")
+print(f"Author: {book['author']}")
+print(f"Pages: {book['pages']}")
+print(f"Available: {book['is_available']}")

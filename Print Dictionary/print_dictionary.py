8e328b4730d9b1cb365d28c40f648e708d54608f
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-06-06 21:19:47 +0800

    Updated Print Dictionary

--- Print Dictionary/print_dictionary.py
+++ Print Dictionary/print_dictionary.py
@@ -10,3 +10,16 @@ print(f"Title: {book['title']}")
 print(f"Author: {book['author']}")
 print(f"Pages: {book['pages']}")
 print(f"Available: {book['is_available']}")
+
+
+book["genre"] = "Software Engineering"
+
+book["is_available"] = False
+
+del book["pages"]
+
+print("\n===== UPDATED BOOK DETAILS =====\n")
+
+for key, value in book.items():
+    print(f"{key.capitalize()}: {value}")
+    

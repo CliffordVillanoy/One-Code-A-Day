30ed26df553a8c6b0d859a10f2d9d3a62675158e
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-06-23 22:29:28 +0800

    List Pattern Updated

--- Lists/list_patterns.py
+++ Lists/list_patterns.py
@@ -10,3 +10,7 @@ print(grades[2:])  # Slicing to get elements from index 2 to the end
 print(grades[1:3])  # Slicing to get elements from index 1 to index 2 (exclusive)       
 print(grades[:2])  # Slicing to get elements from the start to index 1 (exclusive)
 print(grades[-1])  # Accessing the last element using negative indexing
+
+print(grades[-2:])  # Slicing to get the last two elements  
+print(grades[-3:-1])  # Slicing to get elements from the third last to the second last element
+print(grades[-3:])  # Slicing to get the first three elements          

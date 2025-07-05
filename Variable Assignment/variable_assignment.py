03ca9ec8c091f0b19e7449f9b5010ad1ec133cf5
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-07-05 22:48:54 +0800

    Variable Assignment Update

--- Variable Assignment/variable_assignment.py
+++ Variable Assignment/variable_assignment.py
@@ -6,4 +6,10 @@ is_active = True
 full_name = first_name + " " + last_name
 
 print("Full Name:", type (full_name))
+print("First Name:", type(first_name))
+print("Last Name:", type(last_name))
+print("Age:", type(age))
+print("Height:", type(height))
+print("Is Active:", type(is_active))
+print("Full Name:", full_name)
 

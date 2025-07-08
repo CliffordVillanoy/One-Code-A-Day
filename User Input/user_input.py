a52a56c9aba313c4e88e9bd01570e93e3953dd1d
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-07-08 23:35:37 +0800

    User Input Update

--- User Input/user_input.py
+++ User Input/user_input.py
@@ -5,4 +5,11 @@ print (f"Welcome {first_name} {last_name}!")
 # The input() function is used to take input from the user.
 # The f-string is used to format the string and insert the values of first_name and last_name into the string.
 
+print("Data Types:")
+print("First Name:", type(first_name)) 
+print("Last Name:", type(last_name))
+print("Full Name:", type(first_name + " " + last_name))
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", first_name + " " + last_name)
 

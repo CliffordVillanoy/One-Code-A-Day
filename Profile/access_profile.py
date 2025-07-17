5e581a6eebb1206852df60cd40462ba9d491b92a
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-07-17 23:24:35 +0800

    Access Profile Update

--- Profile/access_profile.py
+++ Profile/access_profile.py
@@ -9,5 +9,10 @@ print(user["name" ])
 print(user["interests"][1])
 
 del user["is_active"]
+print(user)
+print(user.get("role", "Role not found"))
+print(user.get("is_active", "Is Active not found"))
+print(user.get("interests", "Interests not found"))
+
 
 

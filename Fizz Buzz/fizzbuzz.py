8a5d8c00be17c6772ca5956037f7c0c6d0faec77
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-08-26 23:49:22 +0800

    Fizz Buzz Update

---
+++ Fizz Buzz/fizzbuzz.py
@@ -0,0 +1,23 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"  # Fix: Added the missing return statement for Buzz
+    else:
+        return str(n)
+
+def test_fizzbuzz():
+    assert fizzbuzz(1) == "1"
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(2) == "2"
+    assert fizzbuzz(6) == "Fizz"
+    assert fizzbuzz(10) == "Buzz"
+    assert fizzbuzz(30) == "FizzBuzz"
+
+print("Please input a number:")
+user_input = int(input())
+print(fizzbuzz(user_input))

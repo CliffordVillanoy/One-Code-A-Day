084ac336e26ba6ee9e3af969232f86c359003d66
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-10-09 17:52:25 +0800

    Print Dictionary Update

--- Print Dictionary/print_dictionary.py
+++ Print Dictionary/print_dictionary.py
@@ -72,6 +72,8 @@ for key, value in third_book.items():
     print("-" * 20)
     print(f"Is '{key}' a list? {isinstance(value, list)}")
     print("-" * 20)
+    print(f"Is '{key}' a dictionary? {isinstance(value, dict)}")
+    print("-" * 20)
 print("===== End of Book Details =====\n")
 
 print("\n===== End of Program =====\n")

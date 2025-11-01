a46a4cbfd4d8bec32a43b154c333cf866cf19c8b
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-11-01 23:20:07 +0800

    Account Simulator Update

--- Account Simulator/account_simulator.py
+++ Account Simulator/account_simulator.py
@@ -58,6 +58,13 @@ for txn in transactions:
         print("\n")
         print("-" * 40)
         print("\n")
+        print("-" * 40)
+        print("\n")
+        print("-" * 40)
+        print("\n")
+        print("-" * 40)
+        print("\n")
+    
 
 
 print("===== END OF TRANSACTION LOG =====")

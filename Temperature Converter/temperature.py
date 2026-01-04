afe419360197efcc4a735c902e71cd7c00f622a5
Clifford Villanoy <villanoy.clifford@gmail.com>
2026-01-05 00:16:30 +0800

    Temperature Converter

---
+++ Temperature Converter/temperature.py
@@ -0,0 +1,98 @@
+def __init__(self):
+        pass
+class TemperatureConverter:
+    def __init__(self):    
+        self.celsius_to_fahrenheit = CelsiusToFahrenheit()
+        self.fahrenheit_to_celsius = FahrenheitToCelsius()
+        self.celsius_to_kelvin = CelsiusToKelvin()
+        self.kelvin_to_celsius = KelvinToCelsius()
+        self.fahrenheit_to_kelvin = FahrenheitToKelvin()
+        self.kelvin_to_fahrenheit = KelvinToFahrenheit()
+class CelsiusToFahrenheit:
+    def __init__(self):
+        pass
+    def convert(self, celsius):
+        return (celsius * 9/5) + 32
+class FahrenheitToCelsius:
+    def __init__(self):
+        pass
+    def convert(self, fahrenheit):
+        return (fahrenheit - 32) * 5/9
+class CelsiusToKelvin:
+    def __init__(self):
+        pass
+    def convert(self, celsius):
+        return celsius + 273.15
+class KelvinToCelsius:
+    def __init__(self):
+        pass
+    def convert(self, kelvin):
+        return kelvin - 273.15
+class FahrenheitToKelvin:
+    def __init__(self):
+        pass
+    def convert(self, fahrenheit):
+        return (fahrenheit - 32) * 5/9 + 273.15
+class KelvinToFahrenheit:
+    def __init__(self):
+        pass
+    def convert(self, kelvin):
+        return (kelvin - 273.15) * 9/5 + 32
+converter = TemperatureConverter()
+celsius = 25
+fahrenheit = converter.celsius_to_fahrenheit.convert(celsius)
+print(f"{celsius}°C is equal to {fahrenheit}°F")
+def __init__(self):
+        pass
+class TemperatureConverter:
+    def __init__(self):    
+        self.celsius_to_fahrenheit = CelsiusToFahrenheit()
+        self.fahrenheit_to_celsius = FahrenheitToCelsius()
+        self.celsius_to_kelvin = CelsiusToKelvin()
+        self.kelvin_to_celsius = KelvinToCelsius()
+        self.fahrenheit_to_kelvin = FahrenheitToKelvin()
+        self.kelvin_to_fahrenheit = KelvinToFahrenheit()
+class CelsiusToFahrenheit:
+    def __init__(self):
+        pass
+    def convert(self, celsius):
+        return (celsius * 9/5) + 32
+class FahrenheitToCelsius:
+    def __init__(self):
+        pass
+    def convert(self, fahrenheit):
+        return (fahrenheit - 32) * 5/9
+class CelsiusToKelvin:
+    def __init__(self):
+        pass
+    def convert(self, celsius):
+        return celsius + 273.15
+class KelvinToCelsius:
+    def __init__(self):
+        pass
+    def convert(self, kelvin):
+        return kelvin - 273.15
+class FahrenheitToKelvin:
+    def __init__(self):
+        pass
+    def convert(self, fahrenheit):
+        return (fahrenheit - 32) * 5/9 + 273.15
+class KelvinToFahrenheit:
+    def __init__(self):
+        pass
+    def convert(self, kelvin):
+        return (kelvin - 273.15) * 9/5 + 32
+converter = TemperatureConverter()
+celsius = 25
+fahrenheit = converter.celsius_to_fahrenheit.convert(celsius)   
+print(f"{celsius}°C is equal to {fahrenheit}°F")
+def __init__(self):
+        pass
+class TemperatureConverter:
+    def __init__(self):    
+        self.celsius_to_fahrenheit = CelsiusToFahrenheit()
+        self.fahrenheit_to_celsius = FahrenheitToCelsius()
+        self.celsius_to_kelvin = CelsiusToKelvin()
+        self.kelvin_to_celsius = KelvinToCelsius()
+        self.fahrenheit_to_kelvin = FahrenheitToKelvin()
+        self.kelvin_to_fahrenheit = KelvinToFahrenheit()
